8a78f9cffd0f38800e5ded5d82529e7874ca5371
Lukas <lupa@itu.dk>
2025-04-04 18:57:11 +0200

    add empty recipe controller

---
+++ src/API/Controllers/RecipeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeController : ControllerBase
+    {
+    }
+}
