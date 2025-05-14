0bcf61a064a01e4ac456579d1c755a72a1929b1b
Lukas <lupa@itu.dk>
2025-05-15 01:30:32 +0200

    ...

--- bachelor_test_file.py
+++ bachelor_test_file.py
@@ -23,7 +23,7 @@ def use_potion(player):
         print(f"{player['name']} unfortunately has no potions left!")
 
 def is_alive(player):
-
+    print("helo")
     return player['hp'] > 0
 
 def player_turn(player, enemy):
